f12e84c331edf8d7f966826ec9f3a15f92003775
Tyler Collins <tk11br@sharcnet.ca>
2020-03-03 14:59:02 -0500

    Reduced supported frameworks

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Andesha/viewclust',
-    version='0.1.0',
+    version='0.1.2',
     zip_safe=False,
 )
+ 
