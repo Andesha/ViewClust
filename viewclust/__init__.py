1c63a3ba7c308b57c3f12d3f9db7c785c011300b
Tyler Collins <tyler.collins@computecanada.ca>
2020-06-24 14:50:38 -0400

    Bug fix for init file

--- viewclust/__init__.py
+++ viewclust/__init__.py
@@ -2,14 +2,9 @@
 from ._version import __version__
 
 # High level functions
-from .job_stack import job_stack
 from .job_use import job_use
 from .node_use import node_use
-from .use_suite import use_suite
 from .cumu_plot import cumu_plot
 from .insta_plot import insta_plot
-from .viol_plot import viol_plot
-from .delta_plot import delta_plot
-from .summary_page import summary_page
 from .target_series import target_series
 from .get_users_run import get_users_run
